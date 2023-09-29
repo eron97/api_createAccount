596ceea962195dfd64d15e40296dc3f23979b8c3
eron97 <eronsantosbetine@gmail.com>
2023-09-29 13:42:14 -0300

    Nova estrutura de projeto

--- internal/api/handlers/createaccount.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "net/http"
-
-func createAccountHandler(w http.ResponseWriter, r *http.Request) {
-	// lógica para lidar com a criação de uma conta.
-	// Pode ler dados JSON de solicitação e persistir os dados
-	// Enviar uma resposta de confirmação
-}
