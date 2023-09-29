4fa9941981b5bb3280b33b6c6f19600a98dca5e1
eron97 <eronsantosbetine@gmail.com>
2023-09-29 13:24:43 -0300

    ---

--- cmd/meu-app/main.go
+++ cmd/meu-app/main.go
@@ -1,23 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func main() {
-
-	// Definir o manipulador (handler) ao endpoint /createaccount
-	http.HandleFunc("/createaccount", createAccountHandler)
-
-	// Porta em que o servidor HTTP irá ouvir
-	port := ":8080"
-
-	// Inicia o servidor HTTP
-	fmt.Printf("Servidor escutando em %s ... \n", port)
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		panic(err)
-	}
-
-}
+func main() {}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module eron97/api_createAccount
+module github.com/eron97/api_createAccount
 
 go 1.21.1
