3c657a73d2e84307265e2e7cd79a4786696f91a3
eron97 <eronsantosbetine@gmail.com>
2023-10-02 14:52:16 -0300

    ok

--- config/controller/createUser.go
+++ config/controller/createUser.go
@@ -1,13 +1,26 @@
 package controller
 
 import (
-	"github.com/eron97/api_createAccount/config/rest_err"
+	"fmt"
+	"log"
+
+	"github.com/eron97/api_createAccount/config/controller/model/request"
+	"github.com/eron97/api_createAccount/config/validation"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Rota chamada de forma errada")
-	c.JSON(err.Code, err)
+	log.Println("Init CreateUser controller")
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		log.Printf("Error trying to marshal objetct, error=%s", err.Error())
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
