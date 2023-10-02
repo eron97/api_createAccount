14904ba7d6ff5c5bed99cef13a0961757f4afef7
eron97 <eronsantosbetine@gmail.com>
2023-10-02 10:19:39 -0300

    Routes ok

--- config/internal/controller/routes/routes.go
+++ config/internal/controller/routes/routes.go
@@ -1,5 +1,14 @@
 package routes
 
-func InitRoutes(r *gin.RouterGroup) {
+import (
+	"github.com/eron97/api_createAccount/config/internal/controller"
+	"github.com/gin-gonic/gin"
+)
 
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/getUserByID/:userID", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
